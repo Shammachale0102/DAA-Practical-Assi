f1a6c791e4b1edbcfbe584dc0b4e6c85843c4360
Sham Machale <120729282+Shammachale0102@users.noreply.github.com>
2023-10-22 20:04:18 +0530

    Update 0-1 knapsack.cpp

--- Assignments/0-1 knapsack.cpp
+++ Assignments/0-1 knapsack.cpp
@@ -1,4 +1,4 @@
-// Assignment 1 : Write a program to solve a 0-1 Knapsack problem using dynamic programming.
+// Assignment 2 : Write a program to solve a 0-1 Knapsack problem using dynamic programming.
 
 
 #include <bits/stdc++.h>
